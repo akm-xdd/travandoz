d73ccbfd2c687fcec06aa29967e87a5547462bbf
code-rishav <raj08rishav@gmail.com>
2022-11-09 21:14:50 +0530

    corrected code for django admin

--- root/urls.py
+++ root/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.home, name='home'),
     path('login',views.login,name='login'),
     path('about',views.about,name='about'),
 ]
--- root/views.py
+++ root/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request,'pages/index.html')
+def home(request):
+    return render(request,'pages/home.html')
 
 def login(request):
     return render(request,'pages/contact.html')
-    
+
 def about(request):
     return render(request,'pages/about.html')
 
